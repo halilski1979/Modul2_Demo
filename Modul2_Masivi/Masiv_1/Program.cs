c89ed752e7b5b91f1d37e5c4a1dc9dcabcfa8c1e
Halil <halilski@abv.bg>
2020-02-22 10:45:36 +0200

    m

---
+++ Modul2_Masivi/Masiv_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masiv_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi den ot sedmicata kato chislo: ");
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days = {"pon", "vto","srq","che","pet","sab","ned"};
+
+            if (day>=1 && day<=7)
+            {
+                Console.WriteLine(days[day-1]);
+            }
+            else
+            {
+                Console.WriteLine("invalid date");
+            }
+            
+        }
+    }
+}
---
+++ Modul2_Masivi/Masivi_VhodIzhod1/Masivi_VhodIzhod1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masivi_VhodIzhod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[5];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"num[{i}]=");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("====FOR=====");
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[{i}]={num[i]}");
+            }
+
+           
+            Console.WriteLine("====FOREACH=====");
+            foreach (var item in num)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("====string.Join=====");
+            Console.WriteLine(string.Join(" ",num));
+        }
+    }
+}
