b9d81d3b34f3a5343259f73120dbbd81d4f178fe
Halil <halilski@abv.bg>
2020-02-26 14:17:25 +0200

    k

---
+++ Spisak/Rechnici_Market/MahaneInterval_pri_SPLIT/MahaneInterval_pri_SPLIT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahaneInterval_pri_SPLIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linia = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Console.WriteLine(string.Join(",", linia));
+        }
+    }
+}
